1cd1b1e38fc3dcd493a8c01a54e5801edd5fc5a7
sfjmt <shoheifjmt@gmail.com>
2018-09-21 16:40:15 +0900

    update

--- src/LightSystem2D.cpp
+++ src/LightSystem2D.cpp
@@ -22,7 +22,6 @@
 //
 // =============================================================================
 
-
 #include "LightSystem2D.h"
 #include "ofGraphics.h"
 #include "ofImage.h"
@@ -30,62 +29,133 @@
 #include "ofAppRunner.h"
 #include "ofEvents.h"
 
-
 namespace ofx {
 
-
-LightSystem2D::LightSystem2D()
-{
-    ofAddListener(ofEvents().setup, this, &LightSystem2D::setup);
-    ofAddListener(ofEvents().update, this, &LightSystem2D::update);
-    ofAddListener(ofEvents().draw, this, &LightSystem2D::draw);
+LightSystem2D::LightSystem2D() {
+    //    ofAddListener(ofEvents().setup, this, &LightSystem2D::setup);
+    //    ofAddListener(ofEvents().update, this, &LightSystem2D::update);
+    //    ofAddListener(ofEvents().draw, this, &LightSystem2D::draw);
     ofAddListener(ofEvents().windowResized, this, &LightSystem2D::windowResized);
 }
 
-
-LightSystem2D::~LightSystem2D()
-{
-    ofRemoveListener(ofEvents().setup, this, &LightSystem2D::setup);
-    ofRemoveListener(ofEvents().update, this, &LightSystem2D::update);
-    ofRemoveListener(ofEvents().draw, this, &LightSystem2D::draw);
+LightSystem2D::~LightSystem2D() {
+    //    ofRemoveListener(ofEvents().setup, this, &LightSystem2D::setup);
+    //    ofRemoveListener(ofEvents().update, this, &LightSystem2D::update);
+    //    ofRemoveListener(ofEvents().draw, this, &LightSystem2D::draw);
     ofRemoveListener(ofEvents().windowResized, this, &LightSystem2D::windowResized);
 }
 
-
-void LightSystem2D::setup(ofEventArgs& args)
-{
+// void LightSystem2D::setup(ofEventArgs& args) {
+//    ofResizeEventArgs resize(ofGetWidth(), ofGetHeight());
+//    windowResized(resize);
+//}
+void LightSystem2D::setup() {
     ofResizeEventArgs resize(ofGetWidth(), ofGetHeight());
     windowResized(resize);
 }
 
+// void LightSystem2D::update(ofEventArgs& args) {
+//    Light2D::List::const_iterator lightIter = _lights.begin();
+//
+//    while (lightIter != _lights.end()) {
+//        (*lightIter)->update();
+//        ++lightIter;
+//    }
+//
+//    Shape2D::List::const_iterator shapeIter = _shapes.begin();
+//
+//    while (shapeIter != _shapes.end()) {
+//        (*shapeIter)->update();
+//        ++shapeIter;
+//    }
+//}
+void LightSystem2D::update() {
+    Light2D::List::const_iterator lightIter = _lights.begin();
 
-void LightSystem2D::update(ofEventArgs& args)
-{
-    for (auto light: _lights) light->update();
-    for (auto shape: _shapes) shape->update();
+    while (lightIter != _lights.end()) {
+        (*lightIter)->update();
+        ++lightIter;
+    }
+
+    Shape2D::List::const_iterator shapeIter = _shapes.begin();
+
+    while (shapeIter != _shapes.end()) {
+        (*shapeIter)->update();
+        ++shapeIter;
+    }
 }
 
-
-void LightSystem2D::draw(ofEventArgs& args)
-{
+// void LightSystem2D::draw(ofEventArgs& args) {
+//    _sceneComp.begin();
+//    ofClear(0, 0, 0, 0);
+//    _sceneComp.end();
+//
+//    Light2D::List::const_iterator lightIter = _lights.begin();
+//
+//    while (lightIter != _lights.end()) {
+//        Shape2D::List::const_iterator shapeIter = _shapes.begin();
+//
+//        _lightComp.begin();
+//        ofClear(0, 0, 0, 0);
+//
+//        (*lightIter)->draw();
+//
+//        ofPushStyle();
+//        ofEnableBlendMode(OF_BLENDMODE_MULTIPLY);
+//        while (shapeIter != _shapes.end()) {
+//            ofMesh mesh;
+//            makeMask(*lightIter, *shapeIter, mesh);
+//            mesh.draw();
+//            ++shapeIter;
+//        }
+//        ofPopStyle();
+//
+//        _lightComp.end();
+//
+//        _sceneComp.begin();
+//        ofPushStyle();
+//        ofEnableBlendMode(OF_BLENDMODE_ADD);
+//        _lightComp.draw(0, 0);
+//        ofPopStyle();
+//        _sceneComp.end();
+//
+//        ++lightIter;
+//    }
+//
+//    _sceneComp.begin();
+//
+//    Shape2D::List::const_iterator shapeIter = _shapes.begin();
+//
+//    while (shapeIter != _shapes.end()) {
+//        (*shapeIter)->draw();
+//        ++shapeIter;
+//    }
+//    _sceneComp.end();
+//
+//    _sceneComp.draw(0, 0);
+//}
+void LightSystem2D::draw() {
     _sceneComp.begin();
     ofClear(0, 0, 0, 0);
     _sceneComp.end();
 
-    for (const auto& light: _lights)
-    {
+    Light2D::List::const_iterator lightIter = _lights.begin();
+
+    while (lightIter != _lights.end()) {
+        Shape2D::List::const_iterator shapeIter = _shapes.begin();
+
         _lightComp.begin();
         ofClear(0, 0, 0, 0);
 
-        light->draw();
+        (*lightIter)->draw();
 
         ofPushStyle();
         ofEnableBlendMode(OF_BLENDMODE_MULTIPLY);
-        for (const auto& shape: _shapes)
-        {
+        while (shapeIter != _shapes.end()) {
             ofMesh mesh;
-            makeMask(light, shape, mesh);
+            makeMask(*lightIter, *shapeIter, mesh);
             mesh.draw();
+            ++shapeIter;
         }
         ofPopStyle();
 
@@ -97,165 +167,129 @@ void LightSystem2D::draw(ofEventArgs& args)
         _lightComp.draw(0, 0);
         ofPopStyle();
         _sceneComp.end();
+
+        ++lightIter;
     }
 
     _sceneComp.begin();
 
-    for (const auto& shape: _shapes) shape->draw();
+    Shape2D::List::const_iterator shapeIter = _shapes.begin();
 
+    while (shapeIter != _shapes.end()) {
+        (*shapeIter)->draw();
+        ++shapeIter;
+    }
     _sceneComp.end();
 
     _sceneComp.draw(0, 0);
-
 }
 
+void LightSystem2D::add(Light2D::SharedPtr light) { _lights.push_back(light); }
 
-void LightSystem2D::add(std::shared_ptr<Light2D> light)
-{
-    _lights.push_back(light);
+void LightSystem2D::add(Shape2D::SharedPtr shape) { _shapes.push_back(shape); }
+
+void LightSystem2D::add(const Light2D::List& lights) { _lights.insert(_lights.end(), lights.begin(), lights.end()); }
+
+void LightSystem2D::add(const Shape2D::List& shapes) { _shapes.insert(_shapes.end(), shapes.begin(), shapes.end()); }
+
+void LightSystem2D::remove(Light2D::SharedPtr light) {
+    Light2D::List::iterator iter = std::find(_lights.begin(), _lights.end(), light);
+
+    if (iter != _lights.end()) {
+        _lights.erase(iter);
+    }
 }
 
+void LightSystem2D::remove(Shape2D::SharedPtr shape) {
+    Shape2D::List::iterator iter = std::find(_shapes.begin(), _shapes.end(), shape);
 
-void LightSystem2D::add(std::shared_ptr<Shape2D> shape)
-{
-    _shapes.push_back(shape);
+    if (iter != _shapes.end()) {
+        _shapes.erase(iter);
+    }
 }
 
+void LightSystem2D::remove(const Light2D::List& lights) {
+    Light2D::List::const_iterator lightIter = lights.begin();
 
-void LightSystem2D::add(const Light2D::List& lights)
-{
-    _lights.insert(_lights.end(), lights.begin(), lights.end());
-}
-
-
-void LightSystem2D::add(const Shape2D::List& shapes)
-{
-    _shapes.insert(_shapes.end(), shapes.begin(), shapes.end());
-}
-
-
-void LightSystem2D::remove(std::shared_ptr<Light2D> light)
-{
-    _lights.erase(std::find(_lights.begin(), _lights.end(), light));
-}
-
-
-void LightSystem2D::remove(std::shared_ptr<Shape2D> shape)
-{
-    _shapes.erase(std::find(_shapes.begin(), _shapes.end(), shape));
-}
-
-
-void LightSystem2D::remove(const Light2D::List& lights)
-{
-    auto lightIter = lights.begin();
-
-    while (lightIter != lights.end())
-    {
+    while (lightIter != lights.end()) {
         remove(*lightIter);
         ++lightIter;
     }
-
 }
 
+void LightSystem2D::remove(const Shape2D::List& shapes) {
+    Shape2D::List::const_iterator shapeIter = shapes.begin();
 
-void LightSystem2D::remove(const Shape2D::List& shapes)
-{
-    auto shapeIter = shapes.begin();
-
-    while (shapeIter != shapes.end())
-    {
+    while (shapeIter != shapes.end()) {
         remove(*shapeIter);
         ++shapeIter;
     }
 }
 
+void LightSystem2D::clearLights() { _lights.clear(); }
 
-void LightSystem2D::clearLights()
-{
-    _lights.clear();
-}
+void LightSystem2D::clearShapes() { _shapes.clear(); }
 
-
-void LightSystem2D::clearShapes()
-{
-    _shapes.clear();
-}
-
-
-void LightSystem2D::makeMask(std::shared_ptr<Light2D> light,
-                             std::shared_ptr<Shape2D> shape,
-                             ofMesh& mask)
-{
-    const auto& poly = shape->getShape();
+void LightSystem2D::makeMask(Light2D::SharedPtr light, Shape2D::SharedPtr shape, ofMesh& mask) {
+    const ofPolyline& poly = shape->getShape();
 
     // Create a list of all poly points that represent a "back facing" edge.
     std::vector<bool> backFacing(poly.size());
 
-    for (std::size_t i = 0; i < poly.size(); ++i)
-    {
+    for (std::size_t i = 0; i < poly.size(); ++i) {
         std::size_t next = (i + 1) % poly.size();
 
-        glm::vec3 firstVertex(poly[i].x, poly[i].y, 0);
+        ofVec2f firstVertex(poly[i].x, poly[i].y);
 
-        glm::vec3 secondVertex(poly[next].x, poly[next].y, 0);
+        ofVec2f secondVertex(poly[next].x, poly[next].y);
 
-        glm::vec3 middle = (firstVertex + secondVertex) / 2;
+        ofVec2f middle = (firstVertex + secondVertex) / 2;
 
-        glm::vec3 lightVector = light->getPosition() - middle;
+        ofVec2f lightVector = light->getPosition() - middle;
 
-        glm::vec3 edgeNormal;
+        ofVec2f edgeNormal;
 
-        edgeNormal.x = - (secondVertex.y - firstVertex.y);
-        edgeNormal.y =    secondVertex.x - firstVertex.x;
+        edgeNormal.x = -(secondVertex.y - firstVertex.y);
+        edgeNormal.y = secondVertex.x - firstVertex.x;
 
-        backFacing[i] = (glm::dot(edgeNormal, lightVector) > 0);
+        backFacing[i] = (edgeNormal.dot(lightVector) > 0);
     }
 
     std::size_t firstBoundaryIndex = std::numeric_limits<std::size_t>::max();
     std::size_t secondBoundaryIndex = std::numeric_limits<std::size_t>::max();
 
-    for (std::size_t current = 0; current < poly.size(); ++current)
-    {
+    for (std::size_t current = 0; current < poly.size(); ++current) {
         std::size_t next = (current + 1) % poly.size();
 
-        if (backFacing[current] != backFacing[next])
-        {
-            if (backFacing[next])
-            {
+        if (backFacing[current] != backFacing[next]) {
+            if (backFacing[next]) {
                 secondBoundaryIndex = next;
-            }
-            else
-            {
+            } else {
                 firstBoundaryIndex = next;
             }
         }
     }
 
-    if (firstBoundaryIndex != std::numeric_limits<std::size_t>::max() &&
-        secondBoundaryIndex != std::numeric_limits<std::size_t>::max())
-    {
+    if (firstBoundaryIndex != std::numeric_limits<std::size_t>::max() && secondBoundaryIndex != std::numeric_limits<std::size_t>::max()) {
         mask.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
 
         int numBoundaryEdges = firstBoundaryIndex - secondBoundaryIndex;
 
-        if (numBoundaryEdges < 0)
-        {
+        if (numBoundaryEdges < 0) {
             numBoundaryEdges += poly.size();
         }
 
-		for(std::size_t offset = 0; offset <= numBoundaryEdges; ++offset)
-        {
-            std::size_t boundaryIndex = (secondBoundaryIndex + offset) % poly.size();
+        for (std::size_t offset = 0; offset <= numBoundaryEdges; ++offset) {
+            int boundaryIndex = (secondBoundaryIndex + offset) % poly.size();
 
-            const auto& boundaryPoint = poly[boundaryIndex];
-            const auto& lightPoistion = light->getPosition();
+            const ofVec2f& boundaryPoint = poly[boundaryIndex];
+            const ofVec2f& lightPoistion = light->getPosition();
 
             // Create normalized ray from the light to the boundary point.
-            auto ray = boundaryPoint - lightPoistion;
+            ofVec2f ray = boundaryPoint - lightPoistion;
 
             // Normalize the ray.
-            ray = glm::normalize(ray);
+            ray.normalize();
 
             // Scale the ray by the light's radius.
             ray *= light->getRadius();
@@ -271,12 +305,9 @@ void LightSystem2D::makeMask(std::shared_ptr<Light2D> light,
     }
 }
 
-
-void LightSystem2D::windowResized(ofResizeEventArgs& resize)
-{
+void LightSystem2D::windowResized(ofResizeEventArgs& resize) {
     _lightComp.allocate(resize.width, resize.height, GL_RGBA);
     _sceneComp.allocate(resize.width, resize.height, GL_RGBA);
 }
 
-
-} // namespace ofx
+}  // namespace ofx
--- src/LightSystem2D.h
+++ src/LightSystem2D.h
@@ -22,9 +22,7 @@
 //
 // =============================================================================
 
-
 #pragma once
-
 
 #include "Light2D.h"
 #include "Shape2D.h"
@@ -32,28 +30,28 @@
 #include "ofShader.h"
 #include "ofFbo.h"
 
-
 namespace ofx {
 
-
-class LightSystem2D
-{
-public:
+class LightSystem2D {
+   public:
     LightSystem2D();
     virtual ~LightSystem2D();
 
-    void setup(ofEventArgs& args);
-    void update(ofEventArgs& args);
-    void draw(ofEventArgs& args);
+    //    void setup(ofEventArgs& args);
+    //    void update(ofEventArgs& args);
+    //    void draw(ofEventArgs& args);
+    void setup();
+    void update();
+    void draw();
 
-    void add(std::shared_ptr<Light2D> light);
-    void add(std::shared_ptr<Shape2D> shape);
+    void add(Light2D::SharedPtr light);
+    void add(Shape2D::SharedPtr shape);
 
     void add(const Light2D::List& light);
     void add(const Shape2D::List& shape);
 
-    void remove(std::shared_ptr<Light2D> light);
-    void remove(std::shared_ptr<Shape2D> shape);
+    void remove(Light2D::SharedPtr light);
+    void remove(Shape2D::SharedPtr shape);
 
     void remove(const Light2D::List& lights);
     void remove(const Shape2D::List& shapes);
@@ -63,18 +61,14 @@ public:
 
     void windowResized(ofResizeEventArgs& resize);
 
-protected:
+   protected:
     Light2D::List _lights;
     Shape2D::List _shapes;
 
     ofFbo _lightComp;
     ofFbo _sceneComp;
 
-    static void makeMask(std::shared_ptr<Light2D> light,
-                         std::shared_ptr<Shape2D> shape,
-                         ofMesh& mask);
-
+    static void makeMask(Light2D::SharedPtr light, Shape2D::SharedPtr shape, ofMesh& mask);
 };
 
-
-} // namespace ofx
+}  // namespace ofx
